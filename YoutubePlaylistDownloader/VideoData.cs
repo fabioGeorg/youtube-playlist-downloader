1b1b4be337818a37ede802dea1f77bd535a7635c
fabioGeorg <62179752+fabioGeorg@users.noreply.github.com>
2021-11-19 01:27:21 +0100

    Add files via upload

---
+++ YoutubePlaylistDownloader/VideoData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YoutubePlaylistDownloader
+{
+    public class VideoData
+    {
+        public string VideoTitle { get; protected set; }
+        public string VideoURL { get; protected set; }
+
+        public VideoData(string videoTitle, string videoUrl)
+        {
+            VideoTitle = videoTitle;
+            VideoURL = videoUrl;
+        }
+
+    }
+}
